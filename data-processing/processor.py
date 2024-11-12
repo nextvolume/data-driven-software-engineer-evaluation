de997352a3ba8f7d8a3226a109537baedf21ebca
Giuseppe Gatta <tails92@gmail.com>
2024-11-12 11:49:04 +0100

    Simple paragraph seperation logic implemented, based on bounding boxes

--- data-processing/processor.py
+++ data-processing/processor.py
@@ -1,4 +1,5 @@
 from py_pdf_parser.loaders import load_file
+from py_pdf_parser.common import BoundingBox
 import sys
 
 def main(argv):
@@ -6,14 +7,46 @@ def main(argv):
         parsePdf(file)
 
 def parsePdf(file):
+    pdf = None
+
     try:
         pdf = load_file(file)
 
-        for el in pdf.elements:
-            print(f"{el.font_name} {el.font_size}")
-            print(el.text())
-
     except:
         print(f"Could not load {file}")
+        return -1
+
+    try:
+        pdf = load_file(file)
+        font_size = 0
+        bounding_box = BoundingBox(-1,-1,-1,-1)
+        parsed = []
+        paragraph = []
+
+        for el in pdf.elements:
+            if bounding_box.y0 - el.bounding_box.y1 > font_size: # New paragraph
+                if len(paragraph) > 0:
+                    parsed.append(paragraph)
+                    paragraph = []
+
+            paragraph.append({"text": el.text(),
+                              "font_name": el.font_name,
+                              "font_size": el.font_size,
+                              "bounding_box": el.bounding_box})
+            
+            font_size = el.font_size
+            bounding_box = el.bounding_box
+
+        if len(paragraph) > 0:
+             parsed.append(paragraph)   
+            
+        for par in parsed:
+            print("*** PARAGRAPH START ***")
+            print(par)
+            print("*** PARAGRAPH END ***")
+
+    except:
+        print(f"Error parsing {file}")
+        return -1
 
 main(sys.argv)
